814b97ae500155ddecd1103c7f2c66ea454d862d
stanno03 <danno_41@hotmail.com>
2023-05-07 16:20:56 +1000

    added GET request for weather data

--- script.js
+++ script.js
@@ -9,40 +9,47 @@
 
 // save that have been selected to local storage and display them.
 
-$( document ).ready(function() {
-    $('.searchbar').on('submit', function() {
+$(document).ready(function() {
+    $(".btn").click(function() {
         // use jquery to get the user value from search from submit. 
-       var cityName = $(this).val()
+       var cityName = $(this).siblings('#searchbar').val();
         // make api call using city name to get long/lat
-        $.ajax({
-            url: "http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=c9b09d7b9d02160b7088c80cf4c4911d",
-            type: GET,
-            success: function(result){
-                var cityData = JSON.parse(result)
-                var lon = cityData[lon]
-                var lat = cityData[lat]
-            }
-        })
-        // use ajax to make api call to open weather map using the long/lat.
-        $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d",
-                type: GET,
-                success: function(result){
-                    var cityData = JSON.parse(result)
 
-                    var icon = cityData[icon]
-                    var temperature = cityData[temp]
-                    var humidity = cityData[humidity]
-                    var windSpeed = cityData[wind.speed]
-
-
+      fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=c9b09d7b9d02160b7088c80cf4c4911d')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                for(i = 0; i < data.length; i ++){
+                    resultsArr = data[i];
                 }
+                var lat = resultsArr.lat
+                var lon = resultsArr.lon
+                console.log(lat)
+                console.log(lon)
+                console.log(resultsArr)
 
-        })
-        
-        
-        
+                fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat +  '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d')
+                .then(res => res.json())
+                .then( data => {
+                    console.log(data)
+                    for(i = 0; i < data.length; i ++){
+                        weatherDataArr = data[i];
+                    }
+    
+                })
+                
+               
+            })
+
+          
+                
+                
+                
+                
+           
         });
         
+
+
     console.log( "ready!" );
 });
