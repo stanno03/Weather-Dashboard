770d6c0d9810ff911e0afe3de30e4ef83956ca5c
stanno03 <danno_41@hotmail.com>
2023-05-15 02:10:55 +1000

    added function to add to list of recent searches

--- script.js
+++ script.js
@@ -1,76 +1,128 @@
 
-// get users location
-// search engine
-// to do: safe to recent 
-// add image
-// polish html/css
+var searchedCityList = [];
+
+function weatherCall(cityName){
+    const apiKey = "c9b09d7b9d02160b7088c80cf4c4911d";
+    $('#current').empty()
+    $('#dayOne').empty()
+    $('#dayTwo').empty()
+    $('#dayThree').empty()
+    $('#dayFour').empty()
+    $('#dayFive').empty()
+    
+       // make an api call using city name to get the lon and lat
+       fetch(`https://api.openweathermap.org/geo/1.0/direct?q=' + ${cityName} + '&limit=1&appid=${apiKey}`)
+       .then(res => res.json())
+       .then(data => {
+           for(let i = 0; i < data.length; i ++){
+               var resultsArr = data[i];
+           }
+           let lat = resultsArr.lat 
+           let lon = resultsArr.lon
+
+           // make api call with the lon and lat to get the current weather data for that city. 
+           fetch('https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + lat +  '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d')
+           .then(res => res.json())
+           .then( data => 
+               {
+                    let weatherIcon = data.weather.icon
+                    console.log(weatherIcon)
+        
+              
+     
+                   let currentWeatherDiv = 
+                   '<p class="p" id="City">' + data.name +  '</p>'+
+                   '<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"></img>'+
+                   '<p class="p" id="Date">' + dayjs.unix(data.dt).format('DD MM YYYY')  +'</p>'+
+                   '<p class="p"id="Temp">'+ "Temp: " + data.main.temp + " &deg;" + "C" +'</p>' +
+                   '<p class="p" id="Wind">'+ "Wind Speed: " + data.wind.speed + " KM/H" +'</p>' +
+                   '<p class="p" id="Humidity">'+ "Humidity: " + data.main.humidity + " %"+'</p>' 
+
+                   $("#current").append(currentWeatherDiv)
+           })
+           // make api call with lon and lat for the 5 day forecast
+           fetch('https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat +  '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d')
+           .then(res => res.json())
+           .then(data => {
+               
+               let dayOneDiv = 
+               '<p class="p" id="DateOne">' + dayjs.unix(data.list[3].dt).format('DD MM YYYY')  +'</p>'+
+               '<p class="p"id="IconOne">'+ "Temp: " + data.list[3].main.temp + " &deg;" + "C" +'</p>' +
+               '<p class="p" id="TempOne">'+ "Wind Speed: " + data.list[3].wind.speed + " KM/H" +'</p>' +
+               '<p class="p" id="HumidityOne">'+ "Humidity: " + data.list[3].main.humidity + " %"+'</p>' 
+
+               $("#dayOne").append(dayOneDiv)
+
+               let dayTwoDiv =
+               '<p class="p" id="DateOne">' + dayjs.unix(data.list[11].dt).format('DD MM YYYY')  +'</p>'+
+               '<p class="p"id="IconOne">'+ "Temp: " + data.list[11].main.temp + " &deg;" + "C" +'</p>' +
+               '<p class="p" id="TempOne">'+ "Wind Speed: " + data.list[11].wind.speed + " KM/H" +'</p>' +
+               '<p class="p" id="HumidityOne">'+ "Humidity: " + data.list[11].main.humidity + " %"+'</p>' 
+
+               $('#dayTwo').append(dayTwoDiv)
+             
+               let dayThreeDiv =
+               '<p class="p" id="DateOne">' + dayjs.unix(data.list[19].dt).format('DD MM YYYY')  +'</p>'+
+               '<p class="p"id="IconOne">'+ "Temp: " + data.list[19].main.temp + " &deg;" + "C" +'</p>' +
+               '<p class="p" id="TempOne">'+ "Wind Speed: " + data.list[19].wind.speed + " KM/H" +'</p>' +
+               '<p class="p" id="HumidityOne">'+ "Humidity: " + data.list[19].main.humidity + " %"+'</p>' 
+
+               $('#dayThree').append(dayThreeDiv) 
+
+               let dayFourDiv = 
+
+               '<p class="p" id="DateOne">' + dayjs.unix(data.list[27].dt).format('DD MM YYYY')  +'</p>'+
+               '<p class="p"id="IconOne">'+ "Temp: " + data.list[27].main.temp + " &deg;" + "C" +'</p>' +
+               '<p class="p" id="TempOne">'+ "Wind Speed: " + data.list[27].wind.speed + " KM/H" +'</p>' +
+               '<p class="p" id="HumidityOne">'+ "Humidity: " + data.list[27].main.humidity + " %"+'</p>' 
+
+               $('#dayFour').append(dayFourDiv)
+
+               let dayFiveDiv = 
+
+               '<p class="p" id="DateOne">' + dayjs.unix(data.list[35].dt).format('DD MM YYYY')  +'</p>'+
+               '<p class="p"id="IconOne">'+ "Temp: " + data.list[35].main.temp + " &deg;" + "C" +'</p>' +
+               '<p class="p" id="TempOne">'+ "Wind Speed: " + data.list[35].wind.speed + " KM/H" +'</p>' +
+               '<p class="p" id="HumidityOne">'+ "Humidity: " + data.list[35].main.humidity + " %"+'</p>' 
+
+               $('#dayFive').append(dayFiveDiv)
+   
+     
+           })
+         })
+     }
+
+function saveCity(cityName){
+    
+    localStorage.setItem("city", cityName)
+}
+
+function getCity(cityName){
+    localStorage.getItem()
+}
+
+function displayRecent(){
+
+}
+
+function addCity(cityName){
+    let cityExists = false;
+        for(let i = 0; i < searchedCityList.length; i ++){
+            if(cityName === searchedCityList[i]){
+                cityExists = true;
+              break;
+            } 
+        } 
+        if(!cityExists){
+            searchedCityList.push(cityName);
+            localStorage.setItem("city", cityName)
+            let cityButton = '<button type="submit" class="btn btn-primary btn-lg col-2">'+ cityName +'</button>'
+            $("#Recent").append(cityButton);
+        }
+}
 
 $(document).ready(function() {
 
-    function weatherCall(cityName){
-        fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=c9b09d7b9d02160b7088c80cf4c4911d')
-        .then(res => res.json())
-        .then(data => {
-            for(let i = 0; i < data.length; i ++){
-                var resultsArr = data[i];
-            }
-            let lat = resultsArr.lat
-            let lon = resultsArr.lon
-      
-            // make api call with the lon and lat to get the current weather data for that city. 
-            fetch('https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + lat +  '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d')
-            .then(res => res.json())
-            .then( data => {
-                
-                let icon = data.weather.icon
-                console.log(data.weather.icon)
-      
-                    $('#City').append(data.name)
-                    $('#Icon').append(src= "https://openweathermap.org/img/w/" + icon + ".png")
-                    $('#Date').append(dayjs.unix(data.dt).format('DD MM YYYY'));
-                    $('#Temp').append(data.main.temp + " &deg;" + "C")
-                    $('#Wind').append(data.wind.speed + " KM/H")
-                    $('#Humidity').append(data.main.humidity + " %")
-                    
-                
-      
-            })
-      
-            fetch('https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat +  '&lon=' + lon + '&appid=c9b09d7b9d02160b7088c80cf4c4911d')
-            .then(res => res.json())
-            .then(data => {
-                
-                
-                console.log(data)
-                    $('#DateOne').append(dayjs.unix(data.list[3].dt).format('DD MM YYYY'));
-                    $('#TempOne').append(data.list[3].main.temp + " &deg;" + "C")
-                    $('#WindOne').append(data.list[3].wind.speed + " KM/H")
-                    $('#HumidityOne').append(data.list[3].main.humidity + " %")
-                    // $('#Wind').append(data.four.wind.speed)
-                    // $('#Humidity').append(data.four.main.humidity)
-                    $('#DateTwo').append(dayjs.unix(data.list[11].dt).format('DD MM YYYY'));
-                    $('#TempTwo').append(data.list[11].main.temp + " &deg;" + "C")
-                    $('#WindTwo').append(data.list[11].wind.speed + " KM/H")
-                    $('#HumidityTwo').append(data.list[11].main.humidity + " %")
-      
-                    $('#DateThree').append(dayjs.unix(data.list[19].dt).format('DD MM YYYY'));
-                    $('#TempThree').append(data.list[19].main.temp + " &deg;" + "C")
-                    $('#WindThree').append(data.list[19].wind.speed + " KM/H")
-                    $('#HumidityThree').append(data.list[19].main.humidity + " %")
-      
-                    $('#DateFour').append(dayjs.unix(data.list[27].dt).format('DD MM YYYY'));
-                    $('#TempFour').append(data.list[27].main.temp + " &deg;" + "C")
-                    $('#WindFour').append(data.list[27].wind.speed + " KM/H")
-                    $('#HumidityFour').append(data.list[27].main.humidity + " %")
-      
-                    $('#DateFive').append(dayjs.unix(data.list[35].dt).format('DD MM YYYY'));
-                    $('#TempFive').append(data.list[35].main.temp + " &deg;" + "C")
-                    $('#WindFive').append(data.list[35].wind.speed + " KM/H")
-                    $('#HumidityFive').append(data.list[35].main.humidity + " %")
-      
-            })
-          })
-      }
 
     $(".btn").on("click", function() {
 
@@ -88,18 +140,21 @@ $(document).ready(function() {
 
     })
 
-
+// add a handler to save searched cities to local storage
     $(".btn").on("click", function(e) {
-        let cityName = $(this).siblings('#searchbar').val();
-        console.log(cityName)
-        
-        let item = <''
-        
+    let cityName = $(this).siblings('#searchbar').val().trim();
+    addCity(cityName)
+    
+   
 
-        $(".recent").append(item);
-     localStorage.setItem(cityName)
+    
+    
 
     })
+
+    
+    
+
 
     console.log( "ready!" );
 });
